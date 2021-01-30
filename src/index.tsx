ae4c0d90e4422a79a8661fe2ba24929853ca1537
Gustavo Harff <gustavo.harff@gmail.com>
2021-01-30 10:45:35 -0300

    feat: configure routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#191622" />
+      <Routes />
+    </NavigationContainer>
+  );
 };
 
 export default App;
