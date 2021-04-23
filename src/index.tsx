5a07fe8b77ecc835a37ff4d8386c4fe12674b292
Gustavo Harff <gustavoharff@insoft4.com>
2021-04-22 22:13:10 -0300

    build: update android version

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,20 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { Routes } from './navigation';
+import { colors } from 'styles';
 
-import { AppProvider } from './hooks';
+import { AppProvider } from 'hooks';
+
+import { Routes } from './navigation';
 
 export default function App(): JSX.Element {
   return (
     <NavigationContainer>
       <AppProvider>
-        <StatusBar barStyle="light-content" backgroundColor="#1B1B1F" />
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={colors.gray[900]}
+        />
         <Routes />
       </AppProvider>
     </NavigationContainer>
